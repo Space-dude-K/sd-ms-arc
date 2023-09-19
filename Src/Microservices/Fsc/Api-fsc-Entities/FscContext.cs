ad9427d90545be3615a9c9920b2a16b47f229d33
Konstantin <88576112+Space-dude-K@users.noreply.github.com>
2023-09-20 00:53:17 +0300

    Fsc context, nlog

---
+++ Src/Microservices/Fsc/Api-fsc-Entities/FscContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api_fsc_Entities
+{
+    public class FscContext : DbContext
+    {
+        public FscContext(DbContextOptions<FscContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.EnableSensitiveDataLogging();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+    }
+}
